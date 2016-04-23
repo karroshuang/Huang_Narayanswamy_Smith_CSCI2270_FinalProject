46274a1b6d42d8114495212c2a2cb95f4302630f
ChaseSmith <chsm5267@colorado.edu>
2016-04-23 15:14:37 -0600

    Added the first method!

--- Catalog.cpp
+++ Catalog.cpp
@@ -1,4 +1,6 @@
 #include "Catalog.h"
+#include <iostream>
+#include <vector>
 
 Catalog::Catalog()
 {
@@ -9,3 +11,33 @@ Catalog::~Catalog()
 {
     //dtor
 }
+void Catalog::printCatalog()
+{
+    item *temp = itemCatalog[x];
+    int counter=0;
+    for(int x = 0; x < tableSize; x++)
+    {
+        while(temp.next != NULL)
+        {
+            counter++;
+            temp = temp->next;
+        }
+    }
+    if(counter == 0)
+    {
+        cout<<"Catalog Empty"<<endl;
+    }
+    else
+    {
+        for(int x = 0; x < tableSize; x++)
+        {
+            temp = itemCatalog[x];
+            cout<<"Category"<<": "<<temp.category<<endl;
+            while(temp->next != NULL)
+            {
+                cout<<"Item:"<<temp.name<<", "<<"Price:"<<temp.price<<endl;
+            }
+        }
+    }
+}
+
