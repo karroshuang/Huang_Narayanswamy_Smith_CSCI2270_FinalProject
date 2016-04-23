d89fea0a83200ce5ca042618046efa6a439d7243
ChaseSmith <chsm5267@colorado.edu>
2016-04-23 16:22:59 -0600

    trying to reconcile differences

--- Catalog.cpp
+++ Catalog.cpp
@@ -36,7 +36,7 @@ void Catalog::printCatalog()
             cout<<"Category"<<": "<<temp->category<<endl;
             while(temp->next != NULL)
             {
-                cout<<"Item:"<<temp->name<<", "<<"Price:"<<temp->price<<", "<<"Quantity:"<<temp->quantity<endl;
+                cout<<"Item:"<<temp->name<<", "<<"Price:"<<temp->price<<", "<<"Quantity:"<<temp->quantity<<endl;
             }
         }
     }
@@ -44,7 +44,7 @@ void Catalog::printCatalog()
 
 void Catalog::printCatalogByPriceRange(int priceRange)
 {
-        int counter=0;
+    int counter=0;
     item *temp = new item;
     for(int x = 0; x < tableSize; x++)
     {
@@ -69,9 +69,48 @@ void Catalog::printCatalogByPriceRange(int priceRange)
             {
                 if(temp->price < priceRange)
                 {
-                    cout<<"Item:"<<temp->name<<", "<<"Price:"<<temp->price<<", "<<"Quantity:"<<temp->quantity<endl;
+                    cout<<"Item:"<<temp->name<<", "<<"Price:"<<temp->price<<", "<<"Quantity:"<<temp->quantity<<endl;
                 }
             }
         }
     }
+}
+
+void Catalog::printCatalogByCategory(string category)
+{
+    int counter=0;
+    item *temp = new item;
+    for(int x = 0; x < tableSize; x++)
+    {
+        temp = itemCatalog[x];
+        while(temp->next != NULL)
+        {
+            counter++;
+            temp = temp->next;
+        }
+    }
+    if(counter == 0)
+    {
+        cout<<"Catalog Empty"<<endl;
+    }
+    else
+    {
+        for(int x = 0; x < tableSize; x++)
+        {
+            temp = itemCatalog[x];
+            if(temp->category == category)
+            {
+                cout<<"Category"<<": "<<temp->category<<endl;
+                while(temp->next != NULL)
+                {
+                        cout<<"Item:"<<temp->name<<", "<<"Price:"<<temp->price<<", "<<"Quantity:"<<temp->quantity<<endl;
+                }
+            }
+        }
+    }
+}
+
+void printCategories()
+{
+
 }
